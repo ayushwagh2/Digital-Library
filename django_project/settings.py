719b40c47a837355641bf9654962ea4eee07398f
ayushwagh2 <69097173+ayushwagh2@users.noreply.github.com>
2023-03-02 15:35:29 +0530

    Third commit

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,9 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import django_heroku
-import dj_database_url
-
+ 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -128,11 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
