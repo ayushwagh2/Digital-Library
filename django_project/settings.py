4c441f870867942fe5532873c6ea249c59f89c57
ayushwagh2 <69097173+ayushwagh2@users.noreply.github.com>
2023-03-03 21:56:45 +0530

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import dj_database_url
  
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -82,9 +81,11 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.parse('postgres://testdb_t1mf_user:A78oTX9NByFo4Z4tioGBq70LdyrxQNRp@dpg-cg0c58hmbg5ek4gmkcsg-a.oregon-postgres.render.com/testdb_t1mf')
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
-
 
 
 # Password validation
